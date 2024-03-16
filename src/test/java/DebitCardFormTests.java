fce3ccbe852f95f5b15e083ea419e423101e8f78
Rraaalf <kymyass@gmail.com>
2024-03-16 19:49:29 +0300

    changed: port

--- src/test/java/DebitCardFormTests.java
+++ src/test/java/DebitCardFormTests.java
@@ -46,7 +46,7 @@ public class DebitCardFormTests {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:7777");
+        driver.get("http://localhost:0000");
     }
 
     @AfterEach
