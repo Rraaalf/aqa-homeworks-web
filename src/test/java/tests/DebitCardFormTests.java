ca0c7f330e3f63d05574736adc6c283969f8010f
Rraaalf <kymyass@gmail.com>
2024-03-16 20:27:53 +0300

    changed: fields are moved to the page class to which they belong

--- src/test/java/tests/DebitCardFormTests.java
+++ src/test/java/tests/DebitCardFormTests.java
@@ -1,3 +1,5 @@
+package tests;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,19 +9,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.DebitCardForm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DebitCardFormTests {
-    private final By NAME_FIELD = By.xpath("//span[@data-test-id='name']//input");
-    private final By NAME_FIELD_MESSAGE = By.xpath("//span[@data-test-id='name']//span[@class='input__sub']");
-    private final By PHONE_FIELD = By.xpath("//span[@data-test-id='phone']//input");
-    private final By PHONE_FIELD_MESSAGE = By.xpath("//span[@data-test-id='phone']//span[@class='input__sub']");
-    private final By CHECKBOX = By.xpath("//label[@data-test-id='agreement']");
-    private final By BUTTON = By.xpath("//button");
-    private final String VALID_NAME = "Иван Петров-Сидоров";
-    private final String VALID_PHONE = "+12345678901";
+    public final String VALID_NAME = "Иван Петров-Сидоров";
+    public final String VALID_PHONE = "+12345678901";
+    private final DebitCardForm debitCardForm = new DebitCardForm();
     private WebDriver driver;
 
     @BeforeAll
@@ -57,10 +55,10 @@ public class DebitCardFormTests {
 
     @Test
     public void positiveTest() {
-        fillField(NAME_FIELD, VALID_NAME);
-        fillField(PHONE_FIELD, VALID_PHONE);
-        clickElement(CHECKBOX);
-        clickElement(BUTTON);
+        fillField(debitCardForm.NAME_FIELD, VALID_NAME);
+        fillField(debitCardForm.PHONE_FIELD, VALID_PHONE);
+        clickElement(debitCardForm.CHECKBOX);
+        clickElement(debitCardForm.BUTTON);
         String successMessage = getMessageText(By.xpath("//p[@data-test-id='order-success']"));
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, successMessage);
@@ -68,52 +66,52 @@ public class DebitCardFormTests {
 
     @Test
     public void invalidNameTest() {
-        fillField(NAME_FIELD, "Ivan");
-        fillField(PHONE_FIELD, VALID_PHONE);
-        clickElement(CHECKBOX);
-        clickElement(BUTTON);
-        String nameFieldMessage = getMessageText(NAME_FIELD_MESSAGE);
+        fillField(debitCardForm.NAME_FIELD, "Ivan");
+        fillField(debitCardForm.PHONE_FIELD, VALID_PHONE);
+        clickElement(debitCardForm.CHECKBOX);
+        clickElement(debitCardForm.BUTTON);
+        String nameFieldMessage = getMessageText(debitCardForm.NAME_FIELD_MESSAGE);
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         assertEquals(expected, nameFieldMessage);
     }
 
     @Test
     public void emptyNameTest() {
-        fillField(PHONE_FIELD, VALID_PHONE);
-        clickElement(CHECKBOX);
-        clickElement(BUTTON);
-        String nameFieldMessage = getMessageText(NAME_FIELD_MESSAGE);
+        fillField(debitCardForm.PHONE_FIELD, VALID_PHONE);
+        clickElement(debitCardForm.CHECKBOX);
+        clickElement(debitCardForm.BUTTON);
+        String nameFieldMessage = getMessageText(debitCardForm.NAME_FIELD_MESSAGE);
         String expected = "Поле обязательно для заполнения";
         assertEquals(expected, nameFieldMessage);
     }
 
     @Test
     public void invalidPhoneTest() {
-        fillField(NAME_FIELD, VALID_NAME);
-        fillField(PHONE_FIELD, "+7657");
-        clickElement(CHECKBOX);
-        clickElement(BUTTON);
-        String phoneFieldMessage = getMessageText(PHONE_FIELD_MESSAGE);
+        fillField(debitCardForm.NAME_FIELD, VALID_NAME);
+        fillField(debitCardForm.PHONE_FIELD, "+7657");
+        clickElement(debitCardForm.CHECKBOX);
+        clickElement(debitCardForm.BUTTON);
+        String phoneFieldMessage = getMessageText(debitCardForm.PHONE_FIELD_MESSAGE);
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         assertEquals(expected, phoneFieldMessage);
     }
 
     @Test
     public void emptyPhoneTest() {
-        fillField(NAME_FIELD, VALID_NAME);
-        clickElement(CHECKBOX);
-        clickElement(BUTTON);
-        String phoneFieldMessage = getMessageText(PHONE_FIELD_MESSAGE);
+        fillField(debitCardForm.NAME_FIELD, VALID_NAME);
+        clickElement(debitCardForm.CHECKBOX);
+        clickElement(debitCardForm.BUTTON);
+        String phoneFieldMessage = getMessageText(debitCardForm.PHONE_FIELD_MESSAGE);
         String expected = "Поле обязательно для заполнения";
         assertEquals(expected, phoneFieldMessage);
     }
 
     @Test
     public void checkboxTest() {
-        fillField(NAME_FIELD, VALID_NAME);
-        fillField(PHONE_FIELD, VALID_PHONE);
-        clickElement(BUTTON);
+        fillField(debitCardForm.NAME_FIELD, VALID_NAME);
+        fillField(debitCardForm.PHONE_FIELD, VALID_PHONE);
+        clickElement(debitCardForm.BUTTON);
         assertTrue(driver.findElement(By.xpath("//label[@data-test-id='agreement'][contains(@class, 'input_invalid')]"))
-                        .isDisplayed());
+                .isDisplayed());
     }
 }
